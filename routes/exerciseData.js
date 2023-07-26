3640f478fe685ba063087451a10ba1ed48b3ba15
pkeen <pkeen7@gmail.com>
2023-07-26 12:41:52 -0600

    add default option for target and equipment select elements

--- routes/exerciseData.js
+++ routes/exerciseData.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const ExerciseData = require('../models/exerciseData');
 
+
+// Search function
+    // Takes target, equipment and bodyPart parameters
+    // queries the db and returns json 
 router.get('/search', async (req, res) => {
     const { target, equipment } = req.query;
     const query = {}
